6f3d2675fafefe4ddb43c5a9ec0af813eff4e60c
your.user.name <jundeptrainhat@gmail.com>
2025-07-16 22:49:06 +0700

    update Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import Login from './components/Login';
 import Register from './components/Register';
+import Profile from './components/Profile';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('/login');
+  const [currentPage, setCurrentPage] = useState('/profile');
+
+  const renderCurrentPage = () => {
+    switch (currentPage) {
+      case '/login':
+        return <Login setCurrentPage={setCurrentPage} />;
+      case '/register':
+        return <Register setCurrentPage={setCurrentPage} />;
+      case '/profile':
+        return <Profile setCurrentPage={setCurrentPage} />;
+      default:
+        return <Login setCurrentPage={setCurrentPage} />;
+    }
+  };
 
   return (
     <div>
-      {currentPage === '/login' ? <Login setCurrentPage={setCurrentPage} /> : <Register setCurrentPage={setCurrentPage} />}
+      {renderCurrentPage()}
     </div>
   );
 }
